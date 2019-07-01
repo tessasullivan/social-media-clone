ffeb9544f3b2382494bb5fa01c8768282e48b0b9
Crystal Fecteau and Tessa Sullivan <tessa@>
2019-07-01 10:44:46 -0700

    Added NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavBar() {
+  var navStyles = {
+    textDecoration: "none",
+    border: "2px solid lightgray",
+    height: "15px",
+    padding: "5px",
+    color: "gray"
+  }
+  return (
+    <div>
+      <a style={navStyles} href="/">Home</a>
+      <a style={navStyles} href="/notifications">Notifications</a>
+      <a style={navStyles} href="/messages">Messages</a>
+    </div>
+  )
+}
+
+export default NavBar;

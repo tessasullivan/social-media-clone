303ad292c4beb61c2ba4a0d830a8e1186f2332d7
Crystal Fecteau and Tessa Sullivan <crystal@>
2019-07-01 11:57:19 -0700

    Added bio box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Body from "./Body";
+import Bio from "./Bio";
 
 function App(){
   return (
     <div>
       <Header/>
       <Body/>
+      <Bio/>
     </div>
   );
 }
---
+++ src/components/Bio.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Bio() {
+  var bioStyle = {
+    height: "300px",
+    width: "225px",
+    border: "2px solid lightgray",
+    padding: "10px",
+    marginTop: "20px"
+  }
+  return (
+    <div style={bioStyle}>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan iaculis elementum. Suspendisse euismod, nibh in fermentum porttitor, lacus velit varius nunc, in dapibus libero lacus a nisl. 
+      </p>
+      <p>
+       Vivamus porta elit pharetra, tincidunt urna ut, euismod metus. Integer a turpis orci. Ut et lacinia leo. Fusce dui neque, tempor id augue et, elementum lacinia elit. 
+      </p>
+    </div>
+  )
+}
+export default Bio;
